5ab6caffa564675a5385da4868165c4fd8a5ef6a
O-BIvan <not@my.mail>
2020-09-25 18:02:54 +0300

    Add track_page.py

---
+++ pages/track_page.py
@@ -0,0 +1,16 @@
+from pages.locators import TrackPageLocators, TrackIds
+
+from pages.base_page import BasePage
+
+
+class TrackPage(BasePage):
+    def should_be_wrong_track_id_message(self):
+        assert self.is_element_present(*TrackPageLocators.WRONG_TRACK_ID_MESSAGE), "Wrong track id message not present"
+
+    def should_be_search_track_id_success(self):
+        barcode = self.browser.find_element(*TrackPageLocators.SEARCH_RESULT_SUCCESS)
+        assert barcode.text == TrackIds.TRACK_ID_WORLD, "Searched track id doesn't match with found one"
+
+    def search_wrong_track_id(self):
+        error_message = self.browser.find_element(*TrackPageLocators.TRACK_ID_INPUT)
+        error_message.send_keys(TrackIds.TRACK_ID_WRONG)
