9ebd7b75ce6cf339ff69ca46ecd30e3d0590e1b2
O-BIvan <not@my.mail>
2020-09-25 21:14:55 +0300

    Change "wrong" to "incorrect" in methods and constants

--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,4 @@
 import pytest
-import time
 
 from pages.track_page import TrackPage
 from pages.main_page import MainPage
@@ -14,14 +13,13 @@ def test_guest_can_track_from_main_page(browser):
     page.search_track_id()
     track_page = TrackPage(browser, browser.current_url)
     track_page.should_be_search_track_id_success()
-    time.sleep(10)
 
 
-@pytest.mark.xfail(reason="Not ready yet - guest won't see wrong id message")
+@pytest.mark.xfail(reason="Not ready yet - guest won't see incorrect id message")
 @pytest.mark.negative
-def test_guest_can_see_wrong_track_id_message_from_main_page(browser):
+def test_guest_can_see_incorrect_track_id_message_from_main_page(browser):
     page = MainPage(browser, link)
     page.open()
-    page.search_wrong_track_id()
+    page.search_incorrect_track_id()
     track_page = TrackPage(browser, browser.current_url)
-    track_page.should_be_wrong_track_id_message()
+    track_page.should_be_incorrect_track_id_message()
