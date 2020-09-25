6932725d4e733a90e6d83e38ec01a4ffa39e4c59
O-BIvan <not@my.mail>
2020-09-25 18:03:20 +0300

    Add main_page.py

---
+++ pages/main_page.py
@@ -0,0 +1,28 @@
+from pages.base_page import BasePage
+from pages.locators import BasePageLocators, TrackIds
+
+# under construction
+
+
+class MainPage(BasePage):
+    def search_track_id(self):
+        self.insert_track_id()
+        self.submit_track_id()
+
+    def insert_track_id(self):
+        input_form = self.browser.find_element(*BasePageLocators.TRACK_ID_INPUT)
+        input_form.send_keys(TrackIds.TRACK_ID_WORLD)
+
+    def submit_track_id(self):
+        submit_button = self.browser.find_element(*BasePageLocators.TRACK_SUBMIT)
+        submit_button.click()
+
+    def search_wrong_track_id(self):
+        input_form = self.browser.find_element(*BasePageLocators.TRACK_ID_INPUT)
+        input_form.send_keys(TrackIds.TRACK_ID_WRONG)
+
+
+
+
+
+
