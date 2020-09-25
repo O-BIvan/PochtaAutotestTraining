360bf551e7ffdbaa5ee888b967a35616e90562fa
O-BIvan <not@my.mail>
2020-09-25 21:15:06 +0300

    Change "wrong" to "incorrect" in methods and constants

--- pages/locators.py
+++ pages/locators.py
@@ -4,7 +4,7 @@ from selenium.webdriver.common.by import By
 class TrackIds:
     TRACK_ID_WORLD = "CA123456789RU"
     TRACK_ID_RUSSIA = "35005145009747"
-    TRACK_ID_WRONG = "123AB23"
+    TRACK_ID_INCORRECT = "123AB23"
 
 
 class BasePageLocators:
@@ -16,5 +16,5 @@ class BasePageLocators:
 class TrackPageLocators:
     TRACK_ID_INPUT = (By.CSS_SELECTOR, ".Input-sc-1jx0q7k-0")  #
     STATUS_MARK = (By.CSS_SELECTOR, ".TrackingCard__CompletedStatus-sc-1fodkqb-4")
-    WRONG_TRACK_ID_MESSAGE = (By.CSS_SELECTOR, ".ErrorLabel-sc-1cl8x16-0")
+    INCORRECT_TRACK_ID_MESSAGE = (By.CSS_SELECTOR, ".ErrorLabel-sc-1cl8x16-0")
     SEARCH_RESULT_SUCCESS = (By.CSS_SELECTOR, ".Barcode__BarcodeLayout-e22oc0-0")
