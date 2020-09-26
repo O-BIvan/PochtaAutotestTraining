9d38d3eaf31ffc0e92f5d8adb5cbc17f773e8bcf
O-BIvan <not@my.mail>
2020-09-26 22:02:21 +0300

    Add LOGIN_BUTTON to MainPageLocators / Remove LOGIN_LINK from BasePageLocators

--- pages/locators.py
+++ pages/locators.py
@@ -2,7 +2,6 @@ from selenium.webdriver.common.by import By
 
 
 class BasePageLocators:
-    LOGIN_LINK = (By.CSS_SELECTOR, "b-header__menu-item--enter")
     TRACK_ID_INPUT = (By.CSS_SELECTOR, "#tracking-number")
     TRACK_SUBMIT = (By.CSS_SELECTOR, ".SearchButton-sc-1dh275s-0")  # генерируется? поменять привязку?
 
@@ -14,16 +13,20 @@ class LoginPageLocators:
 
 class MainPageLocators:
     LOGIN_BUTTON = (By.CSS_SELECTOR, ".b-header__menu-item--enter .b-header__item")
+    BUSINESS_BUTTON = (By.CSS_SELECTOR, ".b-header__menu-item--for-business")
+    BUSINESS_MENU = (By.CSS_SELECTOR, ".b-business-menu__container--visible")
 
 
-class TrackIds:
+class LinkAndId:
     TRACK_ID_WORLD = "CA123456789RU"
     TRACK_ID_RUSSIA = "35005145009747"
     TRACK_ID_INCORRECT = "123AB23"
 
+    LINK_MAIN = "https://www.pochta.ru/"
+
 
 class TrackPageLocators:
-    TRACK_ID_INPUT = (By.CSS_SELECTOR, ".Input-sc-1jx0q7k-0")  #
+    TRACK_ID_INPUT = (By.CSS_SELECTOR, ".Input-sc-1jx0q7k-0")
     STATUS_MARK = (By.CSS_SELECTOR, ".TrackingCard__CompletedStatus-sc-1fodkqb-4")
     INCORRECT_TRACK_ID_MESSAGE = (By.CSS_SELECTOR, ".ErrorLabel-sc-1cl8x16-0")
     SEARCH_RESULT_SUCCESS = (By.CSS_SELECTOR, ".Barcode__BarcodeLayout-e22oc0-0")
