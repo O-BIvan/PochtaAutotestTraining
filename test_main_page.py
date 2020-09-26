91fe01fa793fe3765d5b89c1427490ba3235b9b2
O-BIvan <not@my.mail>
2020-09-26 22:13:17 +0300

    Change import TrackIds to LinkAndId / Add test_guest_can_go_to_login_page and ..open_business_menu

--- test_main_page.py
+++ test_main_page.py
@@ -1,24 +1,42 @@
 import pytest
 
+from pages.locators import LinkAndId
 from pages.track_page import TrackPage
 from pages.main_page import MainPage
-
-link = "https://www.pochta.ru/"
+from pages.login_page import LoginPage
 
 
 @pytest.mark.positive
 def test_guest_can_track_from_main_page(browser):
-    page = MainPage(browser, link)
+    page = MainPage(browser, LinkAndId.LINK_MAIN)
     page.open()
     page.search_track_id()
     track_page = TrackPage(browser, browser.current_url)
     track_page.should_be_search_track_id_success()
 
 
-@pytest.mark.xfail(reason="Not ready yet - guest won't see incorrect id message")
+@pytest.mark.rerun
+@pytest.mark.positive
+def test_guest_can_go_to_login_page(browser):
+    page = MainPage(browser, LinkAndId.LINK_MAIN)
+    page.open()
+    page.go_to_login_page_from_main_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
+
+
+@pytest.mark.positive
+def test_guest_can_open_business_menu(browser):
+    page = MainPage(browser, LinkAndId.LINK_MAIN)
+    page.open()
+    page.open_business_menu()
+    page.should_be_visible_business_menu()
+
+
+@pytest.mark.xfail(reason="Not ready yet - guest won't see incorrect track id message")
 @pytest.mark.negative
 def test_guest_can_see_incorrect_track_id_message_from_main_page(browser):
-    page = MainPage(browser, link)
+    page = MainPage(browser, LinkAndId.LINK_MAIN)
     page.open()
     page.search_incorrect_track_id()
     track_page = TrackPage(browser, browser.current_url)
